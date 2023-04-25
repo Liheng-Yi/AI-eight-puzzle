31d78f0ad73ac7062c3ac005fdbe33e8bf224040
Excalibur0331 <eli1838459978@gmail.com>
2023-04-25 16:14:07 -0700

    finished

--- agent.py
+++ agent.py
@@ -4,72 +4,115 @@ from collections.abc import Callable
 from queue import PriorityQueue
 import numpy as np
 
-'''
-Heuristics
-'''
+
 def MT(board: Board) -> int:
     h = 0
     for i in range(3):
         for j in range(3):
-            if board.state[i][j] != 0:
-                x, y = np.where(board.solution == board.state[i][j])
-                h += abs(i - x) + abs(j - y)
-    return h 
+            if board.state[i][j] != 0 and board.state[i][j] != board.solution[i][j]:
+                h += 1
+    return h
 
 
 def CB(board: Board) -> int:
-    return 
+    '''
+    Counts the number of conflicts in the board's rows and columns
+    '''
+    n = board.state.shape[0]
+    total_conflicts = 0
+    
+    for i in range(n):
+        row = board.state[i]
+        col = board.state[:, i]
+        
+        # Count conflicts in row
+        row_conflicts = 0
+        for j in range(n):
+            if row[j] != 0:
+                for k in range(j + 1, n):
+                    if row[k] != 0 and row[j] > row[k]:
+                        row_conflicts += 1
+        
+        # Count conflicts in column
+        col_conflicts = 0
+        for j in range(n):
+            if col[j] != 0:
+                for k in range(j + 1, n):
+                    if col[k] != 0 and col[j] > col[k]:
+                        col_conflicts += 1
+        
+        # Add total conflicts for row and column to total conflicts for board
+        total_conflicts += row_conflicts + col_conflicts
+    
+    return total_conflicts
+
+
 
 def NA(board: Board) -> int:
-    return 
+    '''
+    Counts the number of conflicts in the board's rows and columns
+    '''
+    n = board.state.shape[0]
+    total_conflicts = 0
+    
+    for i in range(n):
+        row = board.state[i]
+        col = board.state[:, i]
+        
+        # Count conflicts in row
+        row_conflicts = 0
+        for j in range(n):
+            if row[j] != 0:
+                for k in range(j + 1, n):
+                    if row[k] != 0 and row[j] > row[k]:
+                        row_conflicts += 1
+        
+        # Add total conflicts for row and column to total conflicts for board
+        total_conflicts += row_conflicts
+    
+    return total_conflicts
+
+
 
 '''
 A* Search 
 '''
+
+
 def a_star_search(board: Board, heuristic: Callable[[Board], int]):
     frontier = PriorityQueue()
     frontier.put((heuristic(board), board))
     visited = set()
 
-    times = 5766666666666666666222222222222222222222222222222222222222222
+    #times = 100
 
-    while not frontier.empty() and times > 0:
+    while not frontier.empty():# and times > 0:
         _, current_state = frontier.get()
         # print("intitial: \n", current_state.initial_state)
         # print("current_state: \n", current_state.state,"\n")
 
 
-        if current_state.check_solution(current_state.total_action):
-            print("we got this!!!!!!!!!!!, \n current map:\n", current_state)
-
-            return current_state.total_action
-        
         visited.add(tuple(map(tuple, current_state.state)))
 
         next_states = current_state.next_action_states()
-
+        # print("visited: \n", visited)
         for next_state, action in next_states:
-            print("current state: \n", tuple(map(tuple, next_state.state)))
-            # print("visited: \n", visited)
-            print("cost", next_state.g )
+
+            if tuple(map(tuple, current_state.state)) == tuple(map(tuple, current_state.solution)):
+                return current_state.total_action
 
             if tuple(map(tuple, next_state.state)) not in visited:
                 # Calculate the cost of the next state
-                cost = current_state.g + 1 + heuristic(next_state)
-                # print("cost:",cost)
-                # print("action:", action)
-
-
-                # print("visited",visited)
-                # print("next_state:\n",str(next_state))
+                cost = current_state.g + heuristic(next_state)
 
                 # Add the next state to the frontier
                 frontier.put((cost, next_state))
-                
+
                 # Update the cost of the next state
                 next_state.g = current_state.g + 1
                 # append action
                 next_state.total_action.append(action)
-        times = times -1
+
+        #times = times - 1
 
     return None
--- main.py
+++ main.py
@@ -2,21 +2,18 @@ from board import Board
 import numpy as np
 import time
 from agent import MT, a_star_search
+from agent import CB, a_star_search
 
 def main():
 
-    for m in [10]:
+    for m in [10,20,30,40,50]:
         for seed in range(0,10):
             # Sets the seed of the problem so all students solve the same problems
             board = Board(m, seed)
             
-            start =  time.process_time()   
-            '''
-            ***********************************************
-            Solve the Board state here with A*
-            ***********************************************
-            '''
-            solution = a_star_search(board, MT)
+            start =  time.process_time()
+
+            solution = a_star_search(board, CB)
             
             end =  time.process_time()
             solution_cpu_time = end-start
