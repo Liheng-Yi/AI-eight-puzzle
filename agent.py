fbd7497e4207f0205b53266b0a79464d082a8d26
Excalibur0331 <eli1838459978@gmail.com>
2023-04-25 14:18:35 -0700

    testing

--- agent.py
+++ agent.py
@@ -31,25 +31,38 @@ def a_star_search(board: Board, heuristic: Callable[[Board], int]):
     frontier.put((heuristic(board), board))
     visited = set()
 
-    while not frontier.empty():
-        _, current_state = frontier.get()
+    times = 5766666666666666666222222222222222222222222222222222222222222
 
-        if current_state.goal_test():
+    while not frontier.empty() and times > 0:
+        _, current_state = frontier.get()
+        # print("intitial: \n", current_state.initial_state)
+        # print("current_state: \n", current_state.state,"\n")
+
+
+        if current_state.check_solution(current_state.total_action):
+            print("we got this!!!!!!!!!!!, \n current map:\n", current_state)
 
             return current_state.total_action
         
-
-
-        visited.add(current_state)
+        visited.add(tuple(map(tuple, current_state.state)))
 
         next_states = current_state.next_action_states()
+
         for next_state, action in next_states:
-            if str(next_state) not in visited:
+            print("current state: \n", tuple(map(tuple, next_state.state)))
+            # print("visited: \n", visited)
+            print("cost", next_state.g )
+
+            if tuple(map(tuple, next_state.state)) not in visited:
                 # Calculate the cost of the next state
                 cost = current_state.g + 1 + heuristic(next_state)
-                print("cost:",cost)
-                print("action:", action)
-                print("next_state:\n",next_state)
+                # print("cost:",cost)
+                # print("action:", action)
+
+
+                # print("visited",visited)
+                # print("next_state:\n",str(next_state))
+
                 # Add the next state to the frontier
                 frontier.put((cost, next_state))
                 
@@ -57,5 +70,6 @@ def a_star_search(board: Board, heuristic: Callable[[Board], int]):
                 next_state.g = current_state.g + 1
                 # append action
                 next_state.total_action.append(action)
+        times = times -1
 
     return None
--- board.py
+++ board.py
@@ -17,6 +17,7 @@ class Board:
         self._shuffle(m)
         self.initial_state = np.copy(self.state)
         self.g = 0
+        
         self.total_action = []
 
     '''
